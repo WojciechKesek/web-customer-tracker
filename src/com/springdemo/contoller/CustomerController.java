2482949e1ddfbbdf23f7736e3a37ee417f7af73f
Wojciech Kęsek <wojtekesek@gmail.com>
2022-12-15 19:29:14 +0100

    add customer method

--- src/com/springdemo/contoller/CustomerController.java
+++ src/com/springdemo/contoller/CustomerController.java
@@ -6,24 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springdemo.dao.CustomerDAO;
 import com.springdemo.entity.Customer;
+import com.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Customer customer = new Customer();
+		
+		theModel.addAttribute("customer", customer);
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		return "redirect:/customer/list";
 	}
 }
--- src/com/springdemo/dao/CustomerDAOImpl.java
+++ src/com/springdemo/dao/CustomerDAOImpl.java
@@ -19,14 +19,21 @@ public class CustomerDAOImpl implements CustomerDAO{
 
 	
 	@Override
-	@Transactional
 	public List<Customer> getCustomers() {
 		Session session = sessionFactory.getCurrentSession();
 		
-		Query<Customer> theQuery = session.createQuery("from Customer", Customer.class);
+		Query<Customer> theQuery = session.createQuery("from Customer order by lastName", Customer.class);
 		
 		List<Customer> customers = theQuery.getResultList();
+	
 		return customers;
+	}
+
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(customer);
 	}
 
 }
