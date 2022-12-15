67c430fb71066b35894d52ef2e0b65131f70ceb4
Wojciech Kęsek <wojtekesek@gmail.com>
2022-12-15 18:01:53 +0100

    add customer classes and connect to database

--- src/com/springdemo/contoller/CustomerController.java
+++ src/com/springdemo/contoller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.springdemo.contoller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@RequestMapping("/list")
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
 	}
 }
